c5c9b30ba2532f606485ad5a08da40d2940491fa
Liam Monninger <l.mak.monninger@gmail.com>
2025-03-20 18:13:36 -0700

    feat: include kestrel state.

--- kestrel/kestrel/src/lib.rs
+++ kestrel/kestrel/src/lib.rs
@@ -1,4 +1,5 @@
 pub use kestrel_macro::*;
 pub use kestrel_process::*;
+pub use kestrel_state::*;
 /// Kestrel reuses tokio for basic task management.
 pub use tokio::{join, spawn as task, task::JoinHandle, try_join};
